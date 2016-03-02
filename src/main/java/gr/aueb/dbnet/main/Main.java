6116f9049ff6d72216735b5988b4845797a5c06b
Clément Fischer <clement.fischer@outlook.com>
2016-03-02 15:12:38 +0100

    Added a list of parameters to run many tests in a row.

--- src/main/java/gr/aueb/dbnet/main/Main.java
+++ src/main/java/gr/aueb/dbnet/main/Main.java
@@ -2,6 +2,7 @@ package gr.aueb.dbnet.main;
 
 import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Map;
 import java.util.TreeSet;
 
@@ -35,217 +36,55 @@ public class Main {
 		keys.addAll(documentImporter.getData().keySet());
 		// The following line seems to be unnecessary
 		// Collections.sort(keys, DocumentImporter.sortByPreferenceKey);
-		
+
 		// Set isFirstStory
 		TreeSet<String> readTopics = new TreeSet<String>();
 		TDTDocument currentDoc;
-		for (String key : keys){
+		for (String key : keys) {
 			currentDoc = (TDTDocument) documentImporter.getData().get(key);
-			if(readTopics.contains(currentDoc.getLabel())){
+			if (readTopics.contains(currentDoc.getLabel())) {
 				currentDoc.setIsFirstStory(false);
-			}
-			else{
+			} else {
 				currentDoc.setIsFirstStory(true);
 				readTopics.add(currentDoc.getLabel());
 			}
-			
+
 		}
 		// TODO Ask why this is not 250 as it is said in the dataset documentation
 		System.out.println("Number of topics in the dataset: " + readTopics.size());
-		
-		
-		// List of 5 parameters. Each set of parameters starts the a test.
-		EvaluationParameters ep = new EvaluationParameters(60,"b","s","d",0,1);
-		
+
+		LinkedList<EvaluationParameters> listTests = new LinkedList<EvaluationParameters>();
+		// Each set of parameters starts the a test.
+		listTests.add(new EvaluationParameters(60, "b", "s", "d", 0, 1));
+		listTests.add(new EvaluationParameters(60, "n", "s", "d", 0, 1));
+		listTests.add(new EvaluationParameters(60, "k", "b", "d", 0, 1));
+		listTests.add(new EvaluationParameters(60, "b", "b", "u", 0, 1));
+		listTests.add(new EvaluationParameters(60, "k", "s", "u", 0, 1));
+		listTests.add(new EvaluationParameters(60, "l", "b", "n", 0, 1));
+		listTests.add(new EvaluationParameters(60, "k", "s", "n", 0, 1));
+		listTests.add(new EvaluationParameters(60, "k", "b", "n", 0, 1));
+
 		// Add one by one the documents to be processed, give a NS to each document
-		
-		NoveltyScorer ns = new NoveltyScorer(ep);
-		
-		for (String key : keys){
-			currentDoc = (TDTDocument) documentImporter.getData().get(key);
-			try {
-				ns.nextDocument(currentDoc);
-			} catch (ParseException e) {
-				e.printStackTrace();
+		NoveltyScorer ns;
+		CrossValidation cv;
+		for(EvaluationParameters ep : listTests) {
+			ns = new NoveltyScorer(ep);
+			for (String key : keys) {
+				currentDoc = (TDTDocument) documentImporter.getData().get(key);
+				try {
+					ns.nextDocument(currentDoc);
+				} catch (ParseException e) {
+					e.printStackTrace();
+				}
 			}
-			//System.out.println(currentDoc.getNoveltyScore());
+
+			// Cross validation to find the threshold
+			cv = new CrossValidation((Map<String, TDTDocument>) documentImporter.getData(), keys, ep);
+			double avgCost = cv.compute();
+			System.out.println("Parameters:");
+			System.out.println(ep.toString());
+			System.out.println("Average cost: " + avgCost);
 		}
-				
-		// Cross validation to find the threshold
-		// TODO Dangerous cast ?
-		CrossValidation cv = new CrossValidation((Map<String, TDTDocument>) documentImporter.getData(), keys, ep);
-		double avgCost = cv.compute();
-		System.out.println("Parameters:");
-		System.out.println(ep.toString());
-		System.out.println("Average cost: " + avgCost);
-		
-		ep = new EvaluationParameters(60,"n","s","d",0,1);
-		
-		// Add one by one the documents to be processed, give a NS to each document
-		
-		ns = new NoveltyScorer(ep);
-		
-		for (String key : keys){
-			currentDoc = (TDTDocument) documentImporter.getData().get(key);
-			try {
-				ns.nextDocument(currentDoc);
-			} catch (ParseException e) {
-				e.printStackTrace();
-			}
-			//System.out.println(currentDoc.getNoveltyScore());
-		}
-				
-		// Cross validation to find the threshold
-		// TODO Dangerous cast ?
-		cv = new CrossValidation((Map<String, TDTDocument>) documentImporter.getData(), keys, ep);
-		avgCost = cv.compute();
-		System.out.println("Parameters:");
-		System.out.println(ep.toString());
-		System.out.println("Average cost: " + avgCost);
-		
-		ep = new EvaluationParameters(60,"b","b","u",0,1);
-		
-		// Add one by one the documents to be processed, give a NS to each document
-		
-		ns = new NoveltyScorer(ep);
-		
-		for (String key : keys){
-			currentDoc = (TDTDocument) documentImporter.getData().get(key);
-			try {
-				ns.nextDocument(currentDoc);
-			} catch (ParseException e) {
-				e.printStackTrace();
-			}
-			//System.out.println(currentDoc.getNoveltyScore());
-		}
-				
-		// Cross validation to find the threshold
-		// TODO Dangerous cast ?
-		cv = new CrossValidation((Map<String, TDTDocument>) documentImporter.getData(), keys, ep);
-		avgCost = cv.compute();
-		System.out.println("Parameters:");
-		System.out.println(ep.toString());
-		System.out.println("Average cost: " + avgCost);
-		
-		ep = new EvaluationParameters(60,"k","s","u",0,1);
-		
-		// Add one by one the documents to be processed, give a NS to each document
-		
-		ns = new NoveltyScorer(ep);
-		
-		for (String key : keys){
-			currentDoc = (TDTDocument) documentImporter.getData().get(key);
-			try {
-				ns.nextDocument(currentDoc);
-			} catch (ParseException e) {
-				e.printStackTrace();
-			}
-			//System.out.println(currentDoc.getNoveltyScore());
-		}
-				
-		// Cross validation to find the threshold
-		// TODO Dangerous cast ?
-		cv = new CrossValidation((Map<String, TDTDocument>) documentImporter.getData(), keys, ep);
-		avgCost = cv.compute();
-		System.out.println("Parameters:");
-		System.out.println(ep.toString());
-		System.out.println("Average cost: " + avgCost);
-		
-		ep = new EvaluationParameters(60,"l","b","n",0,1);
-		
-		// Add one by one the documents to be processed, give a NS to each document
-		
-		ns = new NoveltyScorer(ep);
-		
-		for (String key : keys){
-			currentDoc = (TDTDocument) documentImporter.getData().get(key);
-			try {
-				ns.nextDocument(currentDoc);
-			} catch (ParseException e) {
-				e.printStackTrace();
-			}
-			//System.out.println(currentDoc.getNoveltyScore());
-		}
-				
-		// Cross validation to find the threshold
-		// TODO Dangerous cast ?
-		cv = new CrossValidation((Map<String, TDTDocument>) documentImporter.getData(), keys, ep);
-		avgCost = cv.compute();
-		System.out.println("Parameters:");
-		System.out.println(ep.toString());
-		System.out.println("Average cost: " + avgCost);
-		
-		ep = new EvaluationParameters(60,"k","b","n",0,1);
-		
-		// Add one by one the documents to be processed, give a NS to each document
-		
-		ns = new NoveltyScorer(ep);
-		
-		for (String key : keys){
-			currentDoc = (TDTDocument) documentImporter.getData().get(key);
-			try {
-				ns.nextDocument(currentDoc);
-			} catch (ParseException e) {
-				e.printStackTrace();
-			}
-			//System.out.println(currentDoc.getNoveltyScore());
-		}
-				
-		// Cross validation to find the threshold
-		// TODO Dangerous cast ?
-		cv = new CrossValidation((Map<String, TDTDocument>) documentImporter.getData(), keys, ep);
-		avgCost = cv.compute();
-		System.out.println("Parameters:");
-		System.out.println(ep.toString());
-		System.out.println("Average cost: " + avgCost);
-		
-		ep = new EvaluationParameters(60,"k","s","d",0,1);
-		
-		// Add one by one the documents to be processed, give a NS to each document
-		
-		ns = new NoveltyScorer(ep);
-		
-		for (String key : keys){
-			currentDoc = (TDTDocument) documentImporter.getData().get(key);
-			try {
-				ns.nextDocument(currentDoc);
-			} catch (ParseException e) {
-				e.printStackTrace();
-			}
-			//System.out.println(currentDoc.getNoveltyScore());
-		}
-				
-		// Cross validation to find the threshold
-		// TODO Dangerous cast ?
-		cv = new CrossValidation((Map<String, TDTDocument>) documentImporter.getData(), keys, ep);
-		avgCost = cv.compute();
-		System.out.println("Parameters:");
-		System.out.println(ep.toString());
-		System.out.println("Average cost: " + avgCost);
-		
-		ep = new EvaluationParameters(60,"k","b","d",0,1);
-		
-		// Add one by one the documents to be processed, give a NS to each document
-		
-		ns = new NoveltyScorer(ep);
-		
-		for (String key : keys){
-			currentDoc = (TDTDocument) documentImporter.getData().get(key);
-			try {
-				ns.nextDocument(currentDoc);
-			} catch (ParseException e) {
-				e.printStackTrace();
-			}
-			//System.out.println(currentDoc.getNoveltyScore());
-		}
-				
-		// Cross validation to find the threshold
-		// TODO Dangerous cast ?
-		cv = new CrossValidation((Map<String, TDTDocument>) documentImporter.getData(), keys, ep);
-		avgCost = cv.compute();
-		System.out.println("Parameters:");
-		System.out.println(ep.toString());
-		System.out.println("Average cost: " + avgCost);
 
 		// Export the results in a csv file
 		// TODO
